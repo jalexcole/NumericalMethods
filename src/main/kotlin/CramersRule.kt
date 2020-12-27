9044a410e8cb0dadf92e550f3e7ca6b60d7aeed8
alex <jalexcole@gmail.com>
2020-12-27 17:07:32 -0600

    Changed the formatting of the package

---
+++ src/main/kotlin/CramersRule.kt
@@ -0,0 +1,20 @@
+
+
+fun cramersRule(matrix: Array<Array<Double>>, b: Array<Double>): Array<Double> {
+    // Assumes a 2d matrix
+
+
+    return arrayOf()
+}
+
+fun determinate(matrix: Array<Array<Double>>): Double {
+
+
+    return 0.0
+}
+
+fun checkIfSquare(matrix: Array<Array<Double>>): Boolean {
+    val x = matrix.size
+    val y = matrix[0].size
+    return x == y
+}
---
+++ src/main/kotlin/CurveFitting/Regression/LinearRegression.kt
@@ -0,0 +1,13 @@
+package CurveFitting.Regression
+
+class LinearRegression {
+
+    constructor(array: Array<Double>) {
+        // Assumes that the interval between the points is one unit
+
+
+    }
+
+
+
+}
