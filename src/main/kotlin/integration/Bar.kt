9eb45ec25ae92c62786728297278fa5598b5ee79
Alex <jalexcole@gmail.com>
2020-12-30 19:42:23 -0600

    Bar integration needs unit tests.

--- src/main/kotlin/integration/Bar.kt
+++ src/main/kotlin/integration/Bar.kt
@@ -1,13 +1,114 @@
 package integration
-/**
- * Bar integration integrates a function using only rectangles derived from the first point of the domain and has a width
- * of the step size. If there are 50 points there will be 49 bars.
- *
- * The area of each bar will be denoted as the height of the left hand point times the step size and the integral value
- * will be the sum of the area of each bar.
- *
- * param
 
+import util.linspace
+
+/**
+ * Bar integration is a crude form of numerical integration where a rectangle is used to calculate the area
+ * between two points.
+ *
+ * @author Alex Cole
+ *
+ * @constructor Takes multiple data types and returns either a double or float value
+ *
  */
 class Bar {
+
+    constructor(dependentValues: Array<Double>, independentValues: Array<Double>) {
+        /**
+         * @param array of f(x) values, x values
+         */
+        integrate(dependentValues, independentValues)
+
+    }
+
+    constructor(dependentValues: Array<Float>, independentValues: Array<Float>) {
+        integrate(dependentValues, independentValues)
+    }
+
+    constructor(function: (Double) -> (Double), domain: Array<Double>, intervals: Int = 50) {
+        var independentValues = arrayOf<Double>()
+        when (domain.size){
+            1 -> {
+                independentValues = linspace(0.0, domain[0], intervals)
+            }
+
+            2 -> {
+                independentValues = linspace(domain[0], domain[1], intervals)
+            }
+        }
+        val dependentValues = arrayListOf<Double>()
+
+        for (i in independentValues) {
+            dependentValues.add(function(i))
+        }
+
+        integrate(dependentValues.toTypedArray(), independentValues)
+
+    }
+
+    constructor(function: (Float) -> (Float), domain: Array<Float>, intervals: Int = 50) {
+        var independentValues = arrayOf<Float>()
+        when (domain.size){
+            1 -> {
+                independentValues = linspace(0.0f, domain[0], intervals)
+            }
+
+            2 -> {
+                independentValues = linspace(domain[0], domain[1], intervals)
+            }
+        }
+        val dependentValues = arrayListOf<Float>()
+
+        for (i in independentValues) {
+            dependentValues.add(function(i))
+        }
+
+        integrate(dependentValues.toTypedArray(), independentValues)
+
+    }
+
+    constructor(dependentValues: Array<Double>) {
+        val independentValues = linspace(0.0, dependentValues.size.toDouble(), dependentValues.size)
+        integrate(dependentValues, independentValues)
+    }
+
+    constructor(dependentValues: Array<Float>) {
+        val independentValues = linspace(0.0f, dependentValues.size.toFloat(), dependentValues.size)
+        integrate(dependentValues, independentValues)
+    }
+
+
+
+    private fun integrate(dependentValues: Array<Double>, independentValues: Array<Double>): Double {
+        var sum = 0.0
+        var barCount = dependentValues.size - 1
+
+        for (i in 0 until barCount){
+            sum = calculateArea(dependentValues[i], independentValues[i])
+        }
+
+        return sum
+    }
+
+    private fun integrate(dependentValues: Array<Float>, independentValues: Array<Float>): Float {
+        var sum = 0.0f
+        var barCount = dependentValues.size - 1
+
+        for (i in 0 until barCount){
+            sum = calculateArea(dependentValues[i], independentValues[i])
+        }
+
+        return sum
+    }
+
+    private fun calculateArea(height: Double, width: Double): Double {
+        return height * width
+    }
+
+    private fun calculateArea(height: Float, width: Float): Float {
+        return height * width
+    }
+
+
 }
+
--- src/main/kotlin/util/Linspace.kt
+++ src/main/kotlin/util/Linspace.kt
@@ -9,3 +9,13 @@ fun linspace(start: Double, stop: Double, stepCount: Int = 50): Array<Double> {
 
         return linspaceArray
     }
+
+fun linspace(start: Float, stop: Float, stepCount: Int = 50): Array<Float> {
+    val step = (stop - start) / stepCount.toFloat()
+    val linspaceArray = arrayOf<Float>()
+    for (i in linspaceArray.indices) {
+        linspaceArray[i] = start + i * step
+    }
+
+    return linspaceArray
+}
